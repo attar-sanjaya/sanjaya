caa9d44760834e6cfa87459bb7637a9f9d6f52cb
Khalief Attar Sanjaya <Khaliefattarsanjaya@gmail.com>
2026-05-06 10:07:16 +0700

    mantao

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    extend: {
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
         display: ['Plus Jakarta Sans', 'sans-serif'],
